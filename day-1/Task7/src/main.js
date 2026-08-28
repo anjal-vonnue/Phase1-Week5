874cd59b45905c01fd83bc23b93fadb94672d249
anjal-vonnue <anjal.biju@vonnue.com>
2026-08-28 10:53:52 +0530

    feat: used different strick modes and demostrated the use of each

--- day-1/Task7/src/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-//link: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-9.html#the-satisfies-operator
-const palette = { primary: "#0D9488" };
-// satisfies helps to retain the property type and helps to us methods on it. it helps to validate certain varible satisfies certain property
